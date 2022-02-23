736df92523b906e3e3c8bf5403c7026a26e86299
Jesse Huang <jesshuang5733@gmail.com>
2022-02-23 09:51:44 -0500

    Create Fish.java

---
+++ 9-Inheritance/9.1/9.1.9/Fish.java
@@ -0,0 +1,12 @@
+public class Fish extends Pet
+{
+    String water;
+    public void setWater(String _water)
+    {
+        this.water = _water;
+    }
+    public String setWater()
+    {
+        return this.water;
+    }
+}
