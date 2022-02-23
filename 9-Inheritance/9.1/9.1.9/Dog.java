210820a57e9fc7e7e4b2ec2f4ec5fca33fc78637
Jesse Huang <jesshuang5733@gmail.com>
2022-02-23 09:51:32 -0500

    Create Dog.java

---
+++ 9-Inheritance/9.1/9.1.9/Dog.java
@@ -0,0 +1,13 @@
+public class Dog extends Pet
+{
+    boolean isTrained;
+    public void setIsTrained(boolean _isTrained)
+    {
+        this.isTrained = _isTrained;
+    }
+    public boolean getIsTrained()
+    {
+        return this.isTrained;
+    }
+   
+}
