7e2467dea4b11b7e018ccc92235dccbf0bd65db5
Jesse Huang <jesshuang5733@gmail.com>
2022-02-23 10:01:21 -0500

    Create Wind.java

---
+++ 9-Inheritance/9.2/9.2.7/Wind.java
@@ -0,0 +1,11 @@
+public class Wind extends Instrument
+{
+    boolean reed;
+    
+    public Wind(String name, String family, boolean r)
+    {
+        super(name, family);
+        reed = r;
+    }
+ 
+}
