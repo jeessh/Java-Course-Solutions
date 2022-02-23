422e3f2ff620838078a9ca425fb296c75941c80b
Jesse Huang <jesshuang5733@gmail.com>
2022-02-23 09:19:15 -0500

    Create 1.4.6.java

---
+++ PrimitiveTypes-1/1.4/1.4.6.java
@@ -0,0 +1,24 @@
+public class MyAge
+{
+    public static void main(String[] args)
+    {
+        int age = 17;
+
+        System.out.print("My current age is: ");
+        System.out.println(age);
+        
+        age++;
+        
+        System.out.print("My age next year will be: ");
+        System.out.println(age);
+        
+        age--;
+        
+        System.out.print("My current age is: ");
+        System.out.print(age);
+    }
+}
+
+//My current age is: 14
+//My age next year will be: 15
+//My current age is: 14
