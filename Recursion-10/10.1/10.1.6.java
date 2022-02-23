a1a7d6bf5e422b28eb59ccfa77634b9d73bc7dde
Jesse Huang <jesshuang5733@gmail.com>
2022-02-23 09:28:34 -0500

    Create 10.1.6.java

---
+++ Recursion-10/10.1/10.1.6.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Factorial
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter a number: ");
+        int number = input.nextInt();
+        
+        System.out.println(calcFactorial(number));
+    }
+    
+    public static int calcFactorial(int x)
+    {
+        // Write a base case
+        if(x > 1) {
+            return x * calcFactorial(x - 1);
+        }
+        else {
+            return 1;
+        }
+    }
+}
