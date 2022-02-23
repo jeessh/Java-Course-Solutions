620b107449c35e1a87af9f674fc45688504f4ad5
Jesse Huang <jesshuang5733@gmail.com>
2022-02-23 09:27:24 -0500

    Create RectangleTester.java

---
+++ UsingObjects-2/2.2/2.2.6/RectangleTester.java
@@ -0,0 +1,12 @@
+public class RectangleTester 
+{
+    public static void main(String[] args)
+    {
+        // Create a rectangle with width 5 and height 12
+        Rectangle newRectangle = new Rectangle(5, 12);
+        // Then print it out
+        
+        
+        System.out.print(newRectangle);
+    }
+}
