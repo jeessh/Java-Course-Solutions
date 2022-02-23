73ee53e48993ef500ac42e8ca082e28695a8b188
Jesse Huang <jesshuang5733@gmail.com>
2022-02-23 09:15:27 -0500

    Create 1.1.9.java

---
+++ PrimitiveTypes-1/1.1/1.1.9.java
@@ -0,0 +1,30 @@
+public class Timeline
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Age  | M/YY - Description");
+        System.out.println("6    | 4/06 - Moved from China to Canada");
+        System.out.println("     |");
+        System.out.println("     |");
+        System.out.println("     |");
+        System.out.println("8    | 7/23 - First visit to Canada's Wonderland");
+        System.out.println("     |");
+        System.out.println("     |");
+        System.out.println("     |");
+        System.out.println("10    | 2/12 - Adopted a highland white terrier dog");
+        System.out.println("     |");
+        System.out.println("     |");
+        System.out.println("     |");
+        System.out.println("12   | 9/27 - Joined first council (student senate)");
+        System.out.println("     |");
+        System.out.println("     |");
+        System.out.println("     |");
+        System.out.println("14   | 6/01 - Nominated as valedictorian of public school");
+        System.out.println("     |");
+        System.out.println("     |");
+        System.out.println("     |");
+        System.out.println("16   | 4/10 - Started first tutoring job");
+
+
+    }
+}
