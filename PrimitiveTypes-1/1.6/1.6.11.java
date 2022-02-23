f7b91a228b5667b0b1f11d47a6e446ae0f08a706
Jesse Huang <jesshuang5733@gmail.com>
2022-02-23 09:22:11 -0500

    Create 1.6.11.java

---
+++ PrimitiveTypes-1/1.6/1.6.11.java
@@ -0,0 +1,15 @@
+public class IntegerOverflow
+{
+    public static void main(String[] args)
+    {
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.MIN_VALUE - 1);
+        System.out.println(Integer.MAX_VALUE + 1);
+    }
+}
+
+//The minimum value for an integer.
+//The maximum value for an integer.
+//The minimum value subtracted by 1.
+//The maximum value with 1 added.
