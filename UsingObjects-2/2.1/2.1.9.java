0e9ed0840a180c1a2e981ecad515b871cc507d37
Jesse Huang <jesshuang5733@gmail.com>
2022-02-23 09:25:45 -0500

    Create 2.1.9.java

---
+++ UsingObjects-2/2.1/2.1.9.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ * The phone class represents a cellular phone.
+ * 
+ * Add the instance variables you think a phone would need
+ */ 
+
+public class Phone
+{
+
+    private String brand;
+    private String typeModel;
+    private int cameraLensNumber;
+    private int appNumber;
+    private int storage; 
+    
+   
+    
+
+}
