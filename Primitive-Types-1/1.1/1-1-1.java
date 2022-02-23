e31bb68468182babace7159d3f7842a91364e6fa
Jesse Huang <jesshuang5733@gmail.com>
2022-02-23 09:10:22 -0500

    Create 1-1-1.java

---
+++ Primitive-Types-1/1.1/1-1-1.java
@@ -0,0 +1,8 @@
+public class Welcome 
+{
+    public static void main(String[] args)
+    {
+        System.out.println("My name is Jesse");
+        System.out.println("I like chinese food");
+    } 
+}
