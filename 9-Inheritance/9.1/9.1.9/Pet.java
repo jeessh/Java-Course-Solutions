ec5ce78968fabf42fceac9b855d405e4ef822310
Jesse Huang <jesshuang5733@gmail.com>
2022-02-23 09:52:05 -0500

    Create Pet.java

---
+++ 9-Inheritance/9.1/9.1.9/Pet.java
@@ -0,0 +1,23 @@
+public class Pet extends Animal
+{
+    private String name;
+    private String size;
+    
+    public void setName(String _name)
+    {
+        this.name = _name;
+    }
+    public String getName()
+    {
+        return this.name;
+    }
+    public void setSize(String _size)
+    {
+        this.size = _size;
+    }
+    public String getSize()
+    {
+        return this.size;
+    }    
+   
+}
