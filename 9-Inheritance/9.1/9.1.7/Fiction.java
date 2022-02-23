59d37d8d43a46dae6933bb2ff1c1cd675df541ef
Jesse Huang <jesshuang5733@gmail.com>
2022-02-23 09:44:31 -0500

    Create Fiction.java

---
+++ 9-Inheritance/9.1/9.1.7/Fiction.java
@@ -0,0 +1,14 @@
+public class Fiction extends Book
+{
+  
+    private String author;
+    
+    
+    public String getAuthor() {
+        return author;
+    }
+    
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}
