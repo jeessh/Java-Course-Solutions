2e55b78089076e08ceffa8ad4445c0c6e9e46cbe
Jesse Huang <jesshuang5733@gmail.com>
2022-02-23 10:26:56 -0500

    Create TicTacToe.java

---
+++ 8-2DArray/8.1/8.1.7/TicTacToe.java
@@ -0,0 +1,17 @@
+public class TicTacToe
+{
+    private String[][] board;
+    
+    public TicTacToe()
+    {
+    this.board = new String[][] {{"-","-","-",},
+                      {"-","-","-",},
+                      {"-","-","-",}};
+    }
+    
+    public String[][] getBoard()
+    {
+        return board;
+    }
+    
+}
