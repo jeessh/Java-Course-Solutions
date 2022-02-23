bed1584c888dd7d6caafbf5f576cddaf28ebdd21
Jesse Huang <jesshuang5733@gmail.com>
2022-02-23 09:49:48 -0500

    Create Computer.java

---
+++ 9-Inheritance/9.1/9.1.8/Computer.java
@@ -0,0 +1,22 @@
+public class Computer
+{
+    private int screenSize;
+    private int memory;
+    
+    public void setScreenSize(int _screenSize)
+    {
+        this.screenSize = _screenSize;
+    }
+    public int getScreenSize()
+    {
+        return this.screenSize;
+    }
+    public void setMemory(int _memory)
+    {
+        this.memory = _memory;
+    }
+    public int getMemory()
+    {
+        return this.memory;
+    }
+}
