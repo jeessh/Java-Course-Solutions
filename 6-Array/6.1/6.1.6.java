4d5188ce5fd510f1cc7b226019a49e3aafc45b89
Jesse Huang <jesshuang5733@gmail.com>
2022-02-23 14:16:21 -0500

    Create 6.1.6.java

---
+++ 6-Array/6.1/6.1.6.java
@@ -0,0 +1,13 @@
+public class FirstArray
+{
+    public static void main(String[] args)
+    {
+    {
+       String stringArray[] = {"h", "e", "l", "l", "o", "w"};
+       for(int i=0; i <= stringArray.length; i++)
+       {
+             System.out.print(stringArray[i]);
+       }
+    }
+    }
+}
