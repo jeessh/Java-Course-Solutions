222b9fd798c5d26a88903963db021d35a9f7ddfb
Jesse Huang <jesshuang5733@gmail.com>
2022-02-23 09:47:51 -0500

    Create Laptop.java

---
+++ 9-Inheritance/9.1/9.1.8/Laptop.java
@@ -0,0 +1,14 @@
+public class Laptop extends Computer
+{
+    private double batteryLife;
+    
+    public void setBatteryLife(double _batteryLife)
+    {
+        this.batteryLife = _batteryLife;
+    }
+    public double getBatteryLife()
+    {
+        return this.batteryLife;
+    }
+   
+}
